4541615394f2fd74d21a38a2951c15636ff81b07
ogunye tofunmi <ogunyeteeholuwar@gmail.com>
2022-03-14 00:20:19 -1200

    first commit

---
+++ src/script4.js
@@ -0,0 +1,13 @@
+const image1 = document.getElementById("image1");
+
+image1.addEventListener("click", (event) => {
+ event.target.style.background = "red"
+ event.target.style.color = "white"
+});
+
+const image2 = document.getElementById("image2");
+
+image2.addEventListener("click", (event) => {
+ event.target.style.background = "red"
+ event.target.style.color = "white"
+});
